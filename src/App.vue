9ab10dc898cb69cf8165f6aad5410c3c75ec285e
R9000P <stephen.yab@gmail.com>
2025-05-02 21:47:01 +0800

    增加新增弹窗

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script lang="ts" setup>
 import {onMounted, ref} from 'vue'
 
-const route = ref('')
-const enterAction = ref({})
-
 onMounted(() => {
   // window.utools.onPluginEnter((action) => {
   //   route.value = action.code
@@ -14,20 +11,40 @@ onMounted(() => {
   // })
 })
 
+// 新增配置项
+const addDialog = ref(false)
 const handleOpenAdd = () => {
-  alert('新增配置项')
+  addDialog.value = true
+}
+const handleAddCancel = () => {
+  addDialog.value = false
+}
+const handleAddSubmit = () => {
+  addDialog.value = false
 }
 </script>
 
 <template>
   <v-card title="Card title" text="..." class="item-card">
     <v-card-actions>
-      <v-btn>Click me</v-btn>
+      <v-btn variant="text" color="red">删除</v-btn>
+      <v-btn variant="text" color="blue">修改</v-btn>
     </v-card-actions>
   </v-card>
 
   <!-- 新增配置项 -->
   <v-btn icon="mdi-plus" class="add-button" @click="handleOpenAdd"/>
+  <v-dialog persistent v-model="addDialog">
+    <v-card title="新增" class="add-card">
+
+      <v-card-actions>
+        <div style="float: right">
+          <v-btn variant="text" color="red" @click="handleAddCancel">取消</v-btn>
+          <v-btn variant="text" color="blue" @click="handleAddSubmit">确定</v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <style scoped>
@@ -39,5 +56,10 @@ const handleOpenAdd = () => {
 
 .item-card {
   margin: 5px;
+  padding: 5px;
+}
+
+.add-card {
+  padding: 5px;
 }
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,7 @@ import 'vuetify/styles'
 import {aliases, mdi} from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css'
 
-import {VBtn, VCard} from 'vuetify/components'
+import {VBtn, VCard, VDialog} from 'vuetify/components'
 
 import {createVuetify} from 'vuetify'
 
@@ -13,6 +13,6 @@ export default createVuetify({
             mdi,
         },
     }, components: {
-        VBtn, VCard
+        VBtn, VCard, VDialog
     }
 })
