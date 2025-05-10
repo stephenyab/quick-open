406cefb40498663cb4b9ddb8767ae183e98f53b9
R9000P <stephen.yab@gmail.com>
2025-05-11 00:06:54 +0800

    设置菜单改版

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -1,36 +1,72 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
+import {computed, ref} from 'vue'
 
-const items = ref([
-  {
-    title: 'WebDav 设置',
-    value: 'web-dav-setting',
-    props: {
-      prependIcon: 'mdi-weather-cloudy',
-      appendIcon: 'mdi-arrow-right-bold'
-    }
-  },
-  {
-    title: '备份',
-    value: 'back',
-    props: {
-      prependIcon: 'mdi-cloud-upload',
-      appendIcon: 'mdi-arrow-right-bold'
-    }
-  },
-  {
-    title: '恢复',
-    value: 'restore',
-    props: {
-      prependIcon: 'mdi-restore',
-      appendIcon: 'mdi-arrow-right-bold'
-    }
+const webDavUrl = ref('')
+
+const webDavAccount = ref('')
+
+const webDavPassword = ref('')
+const webDavPasswordStar = computed(() => {
+  if (webDavPassword.value && webDavPassword.value.length > 0) {
+    return Array(webDavPassword.value.length).fill('*').join('')
+  } else {
+    return ''
   }
-])
+})
+
+const webDavSubFolder = ref('')
+
+const handleOpenAdd = title => {
+
+}
+
+const handleBackup = () => {
+
+}
+
+const handleRestore = () => {
+
+}
 </script>
 
 <template>
-  <v-list class="custom-list" :items="items"></v-list>
+  <v-list>
+    <v-list-item>
+      <v-list-item-title class="text-h5">WebDav 设置</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item @click="handleOpenAdd('WebDav 服务器地址')">
+      <v-list-item-title>WebDav 服务器地址</v-list-item-title>
+      <v-list-item-subtitle>{{ webDavUrl }}</v-list-item-subtitle>
+    </v-list-item>
+
+    <v-list-item @click="handleOpenAdd('WebDav 账号')">
+      <v-list-item-title>WebDav 账号</v-list-item-title>
+      <v-list-item-subtitle>{{ webDavAccount }}</v-list-item-subtitle>
+    </v-list-item>
+
+    <v-list-item @click="handleOpenAdd('WebDav 密码')">
+      <v-list-item-title>WebDav 密码</v-list-item-title>
+      <v-list-item-subtitle>{{ webDavPasswordStar }}</v-list-item-subtitle>
+    </v-list-item>
+
+    <v-list-item @click="handleOpenAdd('子文件夹')">
+      <v-list-item-title>子文件夹</v-list-item-title>
+      <v-list-item-subtitle>{{ webDavSubFolder }}</v-list-item-subtitle>
+    </v-list-item>
+
+    <v-list-item>
+      <v-list-item-title class="text-h5 mt-4">备份与恢复</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item @click="handleBackup">
+      <v-list-item-title>备份</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item @click="handleRestore">
+      <v-list-item-title>恢复</v-list-item-title>
+    </v-list-item>
+  </v-list>
 </template>
 
 <style scoped>
