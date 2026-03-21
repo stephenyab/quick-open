94745e8e46d55f9def4ec57a85482c7fa2b0f9c1
R9000P <stephen.yab@gmail.com>
2026-03-22 00:01:59 +0800

    refactor: 删除未使用的 ConfigItem 组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as EntryDialog } from './EntryDialog.vue'
 export { default as RestoreFileDialog } from './RestoreFileDialog.vue'
-export { default as ConfigItem } from './ConfigItem.vue'
 export { default as WebDavSection } from './WebDavSection.vue'
 export { default as LocalBackupSection } from './LocalBackupSection.vue'
